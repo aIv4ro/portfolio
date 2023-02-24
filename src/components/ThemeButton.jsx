960110a00863abf8c920765ea0d065f17e635bc8
Alvaro <alvaromagu2002@gmail.com>
2023-02-24 19:39:23 +0100

    Add darkTheme

--- src/components/ThemeButton.jsx
+++ src/components/ThemeButton.jsx
@@ -1,8 +1,12 @@
 import { Theme } from "./icons/Theme.jsx";
 
 export function ThemeButton({ }) {
+	const handleClick = (evt) => {
+		evt.preventDefault();
+		document.documentElement.classList.toggle('dark')
+	}
 	return (
-		<button className="transition-colors hover:text-[var(--hover-text-color)]">
+		<button onClick={handleClick} className="transition-colors hover:text-[var(--hover-text-color)]">
 			<Theme className="w-5" />
 		</button>
 	);
