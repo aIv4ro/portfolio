276c726617f375f3e8f366b7298f0552eb7da127
Alvaro <alvaromagu2002@gmail.com>
2023-08-29 20:12:05 +0200

    Migrate mail button to astro

--- src/components/icons/Mail.jsx
+++
@@ -1,9 +0,0 @@
-export function Mail({ width, height }) {
-	return (
-		<svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-mail" width={width} height={height} viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round" >
-			<path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-			<path d="M3 5m0 2a2 2 0 0 1 2 -2h14a2 2 0 0 1 2 2v10a2 2 0 0 1 -2 2h-14a2 2 0 0 1 -2 -2z"></path>
-			<path d="M3 7l9 6l9 -6"></path>
-		</svg >
-	);
-}
