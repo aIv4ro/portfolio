797d4b4cda2d26c5f1fede231c351b95a89c89f1
Alvaro <alvaromagu2002@gmail.com>
2023-08-30 20:44:20 +0200

    eslint init

--- src/types/project.ts
+++ src/types/project.ts
@@ -1,22 +1,22 @@
 export const enum Tecnology {
-	Github = '/github.svg', 
-	Angular = '/angular.svg', 
-	HTML = '/html.svg', 
-	Javascript = '/javascript.svg', 
-	CSS = '/css.svg', 
-	CSharp = '/csharp.svg', 
-	Kotlin = '/kotlin.svg', 
-	NodeJS = '/nodejs.svg', 
-	MongoDB = '/mongodb.svg',
-	React = '/react.svg',
-	Flutter = '/flutter.svg'
+  Github = '/github.svg',
+  Angular = '/angular.svg',
+  HTML = '/html.svg',
+  Javascript = '/javascript.svg',
+  CSS = '/css.svg',
+  CSharp = '/csharp.svg',
+  Kotlin = '/kotlin.svg',
+  NodeJS = '/nodejs.svg',
+  MongoDB = '/mongodb.svg',
+  React = '/react.svg',
+  Flutter = '/flutter.svg'
 }
 
 export interface ProjectI {
-	name: string;
-	description?: string;
-	github: string;
-	web?: string;
-	image: string;
-	technologies: Tecnology[];
+  name: string
+  description?: string
+  github: string
+  web?: string
+  image: string
+  technologies: Tecnology[]
 }
