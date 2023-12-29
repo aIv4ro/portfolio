0ffbaa7ec02499e7610cee8e930b852ff95be04a
Alvaro <alvaromagu2002@gmail.com>
2023-12-29 18:42:36 +0100

    change types in useTranslation

--- src/i18n/i18n.ts
+++ src/i18n/i18n.ts
@@ -2,10 +2,9 @@ import { ui } from './ui'
 
 export const locales = Object.keys(ui)
 export type Locale = keyof typeof ui
-type ValueOf<T> = T[keyof T]
 
-export function useTranslation<T extends Locale> (locale: T): (key: keyof typeof ui[T]) => ValueOf<typeof ui[T]> {
-  return (key: keyof typeof ui[T]): ValueOf<typeof ui[T]> => {
+export function useTranslation<T extends Locale> (locale: T): <V extends keyof typeof ui[T]> (key: V) => typeof ui[T][V] {
+  return <V extends keyof typeof ui[T]> (key: V): typeof ui[T][V] => {
     return ui[locale][key]
   }
 }
