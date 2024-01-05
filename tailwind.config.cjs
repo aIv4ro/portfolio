8c014db7675eece664da2124a1b2ebe6656d8eed
Alvaro <alvaromagu2002@gmail.com>
2024-01-05 13:07:15 +0100

    remove some global styles and use tw features

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,20 @@
 module.exports = {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
-    extend: {}
+    extend: {
+      textColor: {
+        primary: 'black',
+        primaryDark: '#e0dfdf',
+        linkHover: '#0d5dc1',
+        linkHoverDark: '#4497ff'
+      },
+      backgroundColor: {
+        primary: '#f6f6f6',
+        primaryDark: '#2c3333',
+        onPrimary: '#e5e5e5',
+        onPrimaryDark: '#282828'
+      }
+    }
   },
   plugins: [],
   darkMode: 'class'
